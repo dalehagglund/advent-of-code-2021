bcb04fb9c9fb5fa678dab337a41685b26dbfca37
Dale Hagglund <dale.hagglund@gmail.com>
2021-12-05 13:29:00 -0700

    day 5 tdd style

--- 05/thermal_tdd.py
+++ 05/thermal_tdd.py
@@ -1,10 +1,17 @@
 import unittest
 
 class Map:
+    def __init__(self, nrow, ncol):
+        pass
     def count_unsafe(self):
+        return 0
+    def count_at(self, x, y):
         return 0
 
 class TestMap(unittest.TestCase):
     def testEmptyMapHasNoIntersections(self):
-        m = Map()
+        m = Map(10, 10)
         self.assertAlmostEquals(0, m.count_unsafe())
+    def testCountsAreZeroWithEmptyMap(self):
+        m = Map(10, 10)
+        self.assertEquals(0,m.count_at(2, 2))
