a0de7537b9435dbbf51a202780969b13e323d0c9
Dale Hagglund <dale.hagglund@gmail.com>
2021-12-05 13:33:16 -0700

    day 5 tdd style

--- 05/thermal_tdd.py
+++ 05/thermal_tdd.py
@@ -8,10 +8,27 @@ class Map:
     def count_at(self, x, y):
         return 0
 
+class Segment:
+    x0: int
+    y0: int
+    x1: int
+    y1: int
+    def __init__(self, start, end):
+        self.x0, self.y0 = start
+        self.x1, self.y1 = end
+
+class TestSegment(unittest.TestCase):
+    def testInitialization(self):
+        seg = Segment( (0, 2), (4, 6) )
+        self.assertEqual(0, seg.x0)
+        self.assertEqual(2, seg.y0)
+        self.assertEqual(4, seg.x1)
+        self.assertEqual(6, seg.y1)
+
 class TestMap(unittest.TestCase):
     def testEmptyMapHasNoIntersections(self):
         m = Map(10, 10)
-        self.assertAlmostEquals(0, m.count_unsafe())
+        self.assertEqual(0, m.count_unsafe())
     def testCountsAreZeroWithEmptyMap(self):
         m = Map(10, 10)
-        self.assertEquals(0,m.count_at(2, 2))
+        self.assertEqual(0,m.count_at(2, 2))
