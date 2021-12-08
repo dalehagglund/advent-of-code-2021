4b1a0d560af276e794017b96ab2cba07e454bff6
Dale Hagglund <dale.hagglund@gmail.com>
2021-12-07 17:37:43 -0700

    leader: update fetch.py to read details from ini file
    create leaders.ini file (not comitted) that contains
    
        - fetch.template: base url template for aoc leaderboards
        - fetch.session: aoc session cookie
        - fetch.year: year to fetch
    
    and also a "boards" section where each entry is
    
        - board_alias: board id

--- leader/fetch.py
+++ leader/fetch.py
@@ -1,20 +1,52 @@
 import typing as ty
 import sys
 import requests
+from configparser import ConfigParser
+import json
+from functools import partial
 
-urlmap = { ... }
+warn = partial(print, file=sys.stderr)
 
-what = sys.argv[1]
-url = urlmap[what]
-dest = what + ".json"
+def read_config(fname: str) -> ConfigParser:
+    config = ConfigParser()
+    config.read(['leaders.ini'])
+    return config
 
-print(f"fetching {what} from {url}")
+def board_url(config, id) -> str:
+    template = config['fetch']['template']
+    year = config['fetch']['year']
+    return template.format(year=year, id=id)
+
+config = read_config("leaders.ini")
+for section in 'fetch boards'.split():
+    if section not in config:
+        warn(f"fetch: no [{section}] section in leaders.ini")
+        exit(1)
+
+year = 2021
+board = sys.argv[1]
+id = int(config['boards'][board])
+session = config['fetch']['session']
+
+url = board_url(config, id)
+dest = board + ".json"
+
+print(f"year {year} board {board}")
+print(f"from {url}")
+print(f"session {session}")
 print(f"dest {dest}")
 
-r = requests.get(urlmap[what])
+cookies = {'session': session}
+r = requests.get(url, cookies=cookies)
 if r.status_code != 200:
-    print(f"bad status: {r.status_code}")
+    print(f"bad status: {r.status_code}", file=sys.stderr)
     exit(1)
 
+if not r.text.startswith('{'):
+    head = "\n".join(r.text.split("\n")[:25])
+    print(f"bad response: {head}", file=sys.stderr)
+    exit(1)
+
+j = r.json()
 with open(dest, "w") as f:
-    print(r.text, file=f, end=None)
+    print(json.dumps(j, indent=4), file=f)
