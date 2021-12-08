30331bff9c38c80de3db7f1a78158d3d036600ab
Dale Hagglund <dale.hagglund@gmail.com>
2021-12-08 03:19:00 -0700

    08.py: tidying up a bit

--- 08/08.py
+++ 08/08.py
@@ -2,7 +2,7 @@ import typing as ty
 import sys
 from collections import defaultdict
 
-output = [
+segments = [
     "ABCEFG",
     "CF",
     "ACDEG",
@@ -18,7 +18,7 @@ output = [
 display_value = {
     out: value
     for value, out
-    in enumerate(output)
+    in enumerate(segments)
 }
 
 def read_input(fname: str) -> ty.Iterator[ty.Tuple[ty.Set[str], ty.List[str]]]:
@@ -32,7 +32,7 @@ def read_input(fname: str) -> ty.Iterator[ty.Tuple[ty.Set[str], ty.List[str]]]:
 def part1(fname):
     print("===== part 1")
     unique_lengths = {
-        len(output[i]) for i in [1, 4, 7, 8]
+        len(segments[i]) for i in [1, 4, 7, 8]
     }
     count = 0
     for left, right in read_input(fname):
@@ -45,52 +45,41 @@ def solve(left: ty.Set[str], right: ty.List[str]):
     def with_len(n: int) -> ty.Set[str]:
         return { s for s in left if len(s) == n }
 
-    # print(f'{sorted(left) = }')
-        
     def do_singleton(length: int, value: int):
-        word = with_len(length).pop()
-        for wire in word:
-            solution[wire] &= set(output[value])
+        wires = with_len(length).pop()
+        outputs = set(segments[value])
+        for wire in wires:
+            solution[wire] &= outputs
         for wire, sol in solution.items():
-            if wire in word:
+            if wire in wires:
                 continue
-            sol -= set(output[value])
+            sol -= outputs
 
     def do_triple(length: int, values: ty.Set[int]):
-        words = with_len(length)
-        common_words = reduce(set.intersection, [ set(w) for w in words ])
-        common_outputs = reduce(set.intersection, [ set(output[v]) for v in values ])
-        for wire in common_words:
-            solution[wire].intersection_update(common_outputs)
+        common_wires = set.intersection(*[set(w) for w in with_len(length)])
+        common_outputs = set.intersection(*[set(segments[v]) for v in values])
+        for wire in common_wires:
+            solution[wire] &= common_outputs
         singletons = [ w for w, sol in solution.items() if len(sol) == 1 ]
         for wire, sol in solution.items():
             if len(sol) == 1:
                 continue
             for w in singletons:
                 sol -= solution[w]
-            # sol.difference_update(solution[w] for w in singletons)
 
-    do_singleton(2, 1)
-    do_singleton(3, 7)
-    do_singleton(4, 4)
-
-    do_triple(5, {2, 3, 5})
-    do_triple(6, {0, 6, 9})
+    for nseg, value in [ (2, 1), (3, 7), (4, 4) ]:
+        do_singleton(nseg, value)
+    for nseg, values in [ (5, {2, 3, 5}), (6, {0, 6, 9}) ]:
+        do_triple(nseg, values)
 
     for k in solution:
         solution[k] = solution[k].pop()
-    # print(f'{solution = }')
 
     def translate(wire: str) -> int:
-        out = "".join(
-            sorted(solution[w] for w in wire)
-        )
-        return str(display_value[out])
+        segs = "".join(sorted(solution[w] for w in wire))
+        return str(display_value[segs])
     
-    panel = int("".join(
-       translate(wire) for wire in right 
-    ))
-    return panel
+    return int("".join(translate(wire) for wire in right))
 
 def part2(fname):
     print("===== part 2")
