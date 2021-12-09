a20880010cf05d0456280b9b8e9722a0e76b750f
Dale Hagglund <dale.hagglund@gmail.com>
2021-12-08 23:15:34 -0700

    09.py: minor tidying

--- 09/09.py
+++ 09/09.py
@@ -28,12 +28,12 @@ def lowpoints(grid, nrow, ncol):
                 yield row, col 
 def part1(fname):
     nrow, ncol, grid = read_array(fname)
-    sum = 0
+    total_risk = 0
     for row, col in lowpoints(grid, nrow, ncol):
-        sum += grid[row][col] + 1
-    print("part 1:", sum)
+        total_risk += grid[row][col] + 1
+    print("part 1:", total_risk)
 
-def printgrid(grid, nrow, ncol):
+def print_grid(grid, nrow, ncol):
     print("\n".join(
         " ".join(f'{grid[r][c]:3d}' for c in range(ncol))
         for r in range(nrow)
@@ -42,14 +42,11 @@ def printgrid(grid, nrow, ncol):
 def find_basin(tag, basins, grid, nrow, ncol, sr, sc, depth=0):
     basins[sr][sc] = tag
     h = grid[sr][sc]
-    # print("> " * depth, f"{sr = } {sc = }")
+
     yield sr, sc
     for nr, nc in neighbours(nrow, ncol, sr, sc):
-        # print("> " * depth, f"{nr = } {nc = }")
-        if grid[nr][nc] == 9:
-            continue
-        if basins[nr][nc] == tag:
-            continue
+        if grid[nr][nc] == 9: continue
+        if basins[nr][nc] == tag: continue
         if grid[nr][nc] > h:
             yield from find_basin(tag, basins, grid, nrow, ncol, nr, nc, depth = depth+1)
       
@@ -60,26 +57,13 @@ def part2(fname):
         [0] * ncol 
         for _ in range(nrow)
     ]
-    # printgrid(grid, nrow, ncol)
-
-    for row in range(nrow):
-        for col in range(ncol):
-            if grid[row][col] == 9:
-                basins[row][col] = -1
-
-    # print(list(find_basin(3, basins, grid, nrow, ncol, 0, 1)))
-    # for br, bc in find_basin(3, basins, grid, nrow, ncol, 0, 0):
-    #     print((br, bc)
-
 
     lows = lowpoints(grid, nrow, ncol)
     sizes = [
         len(list(find_basin(i + 1, basins, grid, nrow, ncol, lr, lc)))
         for i, (lr, lc) in enumerate(lows)
     ]
-    # for i, (row, col) in enumerate(lowpoints(grid, nrow, ncol)):
-    #     cells = list(find_basin(i + 1, basins, grid, nrow, ncol, row, col))
-    #     print(f'lowpoint ({row}, {col}) basin size {len(cells)}')
+
     from operator import mul
     from functools import reduce
     print("part 2:", reduce(mul, sorted(sizes)[-3:]))
