fe918e7e60b5104c19a8dbaffd1bc6d7a4cbfa5f
Dale Hagglund <dale.hagglund@gmail.com>
2021-12-09 05:16:14 -0700

    09.py: more tidying

--- 09/09.py
+++ 09/09.py
@@ -14,18 +14,20 @@ def neighbours(nrow, ncol, row, col):
     if col < ncol - 1: yield row, col + 1
     if row < nrow - 1: yield row + 1, col
 
-def lowpoints(grid, nrow, ncol):
+def low_points(grid, nrow, ncol):
     for row in range(nrow):
         for col in range(ncol):
             if all(
                 grid[row][col] < grid[nr][nc]
-                for nr, nc in neighbours(nrow, ncol, row, col)
+                for nr, nc
+                in neighbours(nrow, ncol, row, col)
             ):
                 yield row, col 
+
 def part1(fname):
     nrow, ncol, grid = read_array(fname)
     total_risk = 0
-    for row, col in lowpoints(grid, nrow, ncol):
+    for row, col in low_points(grid, nrow, ncol):
         total_risk += grid[row][col] + 1
     print("part 1:", total_risk)
 
@@ -34,6 +36,9 @@ def print_grid(grid, nrow, ncol):
         " ".join(f'{grid[r][c]:3d}' for c in range(ncol))
         for r in range(nrow)
     ))
+
+def make_grid(nrow, ncol, fill=0):
+    return [ [fill] * ncol for _ in range(nrow) ]
 
 def find_basin(tag, basins, grid, nrow, ncol, sr, sc, depth=0):
     basins[sr][sc] = tag
@@ -48,21 +53,19 @@ def find_basin(tag, basins, grid, nrow, ncol, sr, sc, depth=0):
       
 def part2(fname):
     nrow, ncol, grid = read_array(fname)
-    sizes = {}
-    basins = [ 
-        [0] * ncol 
-        for _ in range(nrow)
-    ]
-
-    lows = lowpoints(grid, nrow, ncol)
-    sizes = [
-        len(list(find_basin(i + 1, basins, grid, nrow, ncol, lr, lc)))
+    basins = make_grid(nrow, ncol, 0)
+    
+    lows = low_points(grid, nrow, ncol)
+    sizes = sorted(
+        len(list(
+            find_basin(
+                i + 1, basins, grid, nrow, ncol, lr, lc)))
         for i, (lr, lc) in enumerate(lows)
-    ]
+    )
 
     from operator import mul
     from functools import reduce
-    print("part 2:", reduce(mul, sorted(sizes)[-3:]))
+    print("part 2:", reduce(mul, sizes[-3:]))
 
 if __name__ == '__main__':
     part1(sys.argv[1])        
