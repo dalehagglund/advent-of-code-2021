e28787a56cff51034c73503cdc8e9a5d1cbafc8a
Dale Hagglund <dale.hagglund@gmail.com>
2021-12-09 23:12:18 -0700

    day 10

---
+++ 10/10.py
@@ -0,0 +1,100 @@
+import sys
+
+class Illegal(Exception): pass
+class Incomplete(Exception): pass
+
+closes = {
+    '(': ')',
+    '[': ']',
+    '<': '>',
+    '{': '}',
+}
+
+trace = lambda *args: None
+def bal(s, end, depth=0):
+    trace(f'bal: {end = }')
+    while True:
+        i, c = next(s)
+        trace(f"bal: {i = } {c = }")
+        if c == '\n' and depth > 0:
+            trace("#a")
+            raise Incomplete("")
+        elif c == '\n':
+            trace("#b")
+            return ""
+        elif c == end:
+            trace("#c")
+            return end
+        elif c in closes:
+            trace("#d")
+            try:
+                bal(s, closes[c], depth+1)
+            except Incomplete as exc:
+                raise Incomplete(exc.args[0] + closes[c])
+        else:
+            trace("#e")
+            raise Illegal(c)
+
+def read_input(fname):
+    with open(fname) as f:
+        for line in f:
+            yield line
+
+test_input = [ 
+    # "()\n",
+    # "<>\n",
+    # "[]\n",
+    # "{}\n",
+    # "(())\n",
+    # "()<>\n",
+    # "([]{})\n",
+    "(]\n",
+]
+
+syntax_scores = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+}
+
+incomplete_scores = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+}
+
+def part1(fname):
+    lines = read_input(fname)
+    syntax_score = 0
+    completion_scores = []
+
+    for line in lines:
+        completion_score = 0
+        # print(f"---> {line.strip()}")
+        try:
+            closing = bal(enumerate(iter(line)), "\n")
+            assert closing == "", f"huh? {closing =}"
+        except Illegal as exc:
+            c = exc.args[0]
+            syntax_score += syntax_scores[c]
+        except Incomplete as exc:
+            for c in exc.args[0]:
+                completion_score *= 5
+                completion_score += incomplete_scores[c]
+            print(f'{completion_score = }')
+            completion_scores.append(completion_score)
+        
+    print(f'part 1: {syntax_score}')
+
+    completion_scores.sort()
+    mid = len(completion_scores) // 2
+    print(f'part 2: {completion_scores[mid]}')
+
+def part2(fname):
+    print('part 2:')
+
+if __name__ == '__main__':
+    part1(sys.argv[1])
+    # part2(sys.argv[1])
